6eac189c2f2b1a4a7aef15d0aec26450a0469020
simple_man <markevich.roma@gmail.com>
2018-08-07 02:36:24 +0500

    test

--- Saper/FormSaper.Designer.cs
+++ Saper/FormSaper.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Saper
 {
-    partial class Form1
+    partial class FormSaper
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormSaper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormSaper";
+            this.Text = "Сапер";
+            this.Load += new System.EventHandler(this.FormSaper_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Saper/FormSaper.cs
+++ Saper/FormSaper.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Saper
 {
-    public partial class Form1 : Form
+    public partial class FormSaper : Form
     {
-        public Form1()
+        public FormSaper()
         {
             InitializeComponent();
+        }
+
+        private void FormSaper_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
