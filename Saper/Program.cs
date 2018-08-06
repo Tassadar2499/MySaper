376d39447242b4497878fe482c47b62f55b6f82e
simple_man <markevich.roma@gmail.com>
2018-08-07 02:31:41 +0500

    segf

--- Saper/Program.cs
+++ Saper/Program.cs
@@ -11,7 +11,7 @@ namespace Saper
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
-        [STAThread]
+        [STAThread]//
         static void Main()
         {
             Application.EnableVisualStyles();
